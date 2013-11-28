77b5556dedb7b93396c19a38a7a7b61e2ced6092
Christian Kellner <christian@kellner.me>
2013-11-28 14:48:24 +0100

    Move most of the conference specifics into config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+config = {
+    "title"   : "Bernstein Conference 2013 - Abstracts",
+    "short"   : "BC13",
+    "long"    : "Bernstein Conference 2013",
+    "cite"    : "Bernstein Conference 2013, T&uuml;bingen, Germany, September 24 - 27",
+    "logo"    : "bc13logo.png",
+    "data"    : "bc13data.js",
+    "home"    : "http://bernstein-conference.de",
+    "doiLink" : "https://portal.g-node.org/abstracts/bc13/#",
+    "doiPref" : "10.12751",
+
+    "groups" : [
+	{ "id" : 0,
+	  "short" : "I",
+	  "long": "Invited Talks"
+	},
+	{ "id" : 1,
+	  "short" : "C",
+	  "long": "Contributed Talks"
+	},
+	{ "id" : 2,
+	  "short" : "W",
+	  "long": "Posters Wednesday"
+	},
+	{ "id" : 3,
+	  "short" : "T",
+	  "long": "Posters Thursday"
+	},
+	{ "id" : 4,
+	  "short" : "S",
+	  "long": "Workshop"
+	}
+    ]
+};
