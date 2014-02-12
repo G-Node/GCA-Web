5f27f2a5cfcba8d7ded397a393fe4019142c528a
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-02-12 15:46:27 +0100

    Model for accounts

---
+++ app/service/models/Account.scala
@@ -0,0 +1,21 @@
+// Copyright © 2014, German Neuroinformatics Node (G-Node)
+//                   A. Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package service.models
+
+/**
+ * Model class
+ */
+class Account {
+
+  var uuid: String
+  var mail: String
+  var abstracts: Seq[Abstracts]
+
+}
