296020efbd81c323dc2526472b505e2e0ab5d110
Christian Kellner <christian@kellner.me>
2014-02-13 16:18:48 +0100

    [auth] add two sample pages, one user-aware and one secured

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,23 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+object Application extends Controller with securesocial.core.SecureSocial {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def showUserInfo = UserAwareAction { implicit request =>
+    val userName = request.user match {
+      case Some(user) => user.fullName
+      case _ => "guest"
+    }
+
+    Ok("Hello %s".format(userName))
+  }
+
+  def showSecret = SecuredAction { implicit request =>
+    Ok("The answer is 42.")
   }
 
 }
