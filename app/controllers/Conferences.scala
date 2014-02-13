1269f936389e2cd838261715d2fe8c4473c0fb6a
Andrey <sobolev.andrey@gmail.com>
2014-02-13 16:42:09 +0100

    changes to routes + a trait for the conferences controller added

---
+++ app/controllers/Conferences.scala
@@ -0,0 +1,61 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.json._
+//import service.ConferenceService
+import models.Conference
+
+/**
+ * Conferences controller.
+ * Manages HTTP request logic for conferences.
+ */
+
+object Conferences extends Controller {
+
+  /**
+   * Create a new conference.
+   *
+   * @return new conference in JSON / Redirect to the conference page
+   */
+  def create : Action[AnyContent] = TODO
+
+  /**
+   * List all available conferences.
+   *
+   * @return All conferences publicly available.
+   */
+  def list: Action[AnyContent] = Action { request =>
+    Ok(views.html.index("Your new application is ready."))
+    //render {
+    //  case "text/html" => Ok(views.html.index("Your new application is ready."))
+    //  case "application/json" => Ok(Json.toJson("Your new application is ready.").toString())
+  }
+
+  /**
+   * A conference info by id.
+   *
+   * @param id The id of the conference.
+   *
+   * @return A conference as JSON / page with conference info.
+   */
+  def get(id: String) : Action[AnyContent] = TODO
+
+  /**
+   * Update an existing conference info.
+   *
+   * @param id   The Abstract to update.
+   *
+   * @return conference in JSON / conference page
+   */
+  def update(id: String) : Action[AnyContent] = TODO
+
+  /**
+   * Update an existing conference info.
+   *
+   * @param id   Conference id to delete.
+   *
+   * @return OK or Failed / Redirect to the conferences list page
+   */
+  def delete(id: String) : Action[AnyContent] = TODO
+
+}
