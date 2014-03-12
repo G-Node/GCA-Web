020a8d3bd3c82de82c3c157e63b87184721e3594
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-03-12 14:02:17 +0100

    editor.js: basic editor without post request

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -3,34 +3,38 @@ define(["lib/models", "lib/tools"], function(models, tools) {
 
     function EditorViewModel(confId, abstrId) {
 
-        if (tools.isGlobalOrUndefined(this) || this === undefined) {
+
+        if (! (this instanceof EditorViewModel)) {
             return new EditorViewModel(confId, abstrId);
         }
 
         var self = this;
 
+        self.abstractSaved = ko.observable(false);
         self.abstract = ko.observable();
         self.conference = ko.observable();
 
         self.run = function() {
-            $(document).ready(function() {
-                console.log(confId);
-                console.log(abstrId);
+            console.log(confId);
+            console.log(abstrId);
 
-                if (confId) {
-                    self.getConference(confId);
-                }
+            if (confId) {
+                self.getConference(confId);
+                self.abstract(models.Abstract());
+            } else if (abstrId) {
+                self.getAbstract(abstrId);
+                self.abstractSaved(true);
+            } else {
+                throw "Conference id or abstract id must be defined";
+            }
 
-                if (abstrId) {
-
-                }
-            });
+            ko.applyBindings(self);
         };
 
         self.getConference = function(confId) {
 
             $.ajax({
-                async: true,
+                async: false,
                 url: "/conferences/" + confId,
                 type: "GET",
                 success: success,
@@ -39,16 +43,45 @@ define(["lib/models", "lib/tools"], function(models, tools) {
             });
 
             function success(obj, stat, xhr) {
-                self.conference = models.Conference.fromObject(obj);
-                var foo = "foo";
-                console.log(foo);
-                self.abstract = foo;
+                self.conference(models.Conference.fromObject(obj));
             }
 
             function fail(xhr, stat, msg) {
                 console.log("Error while requesting the conference: uuid = " + confId);
             }
 
+        };
+
+        self.getAbstract = function(abstrId) {
+
+            $.ajax({
+                async: false,
+                url: "/abstracts/" + confId,
+                type: "GET",
+                success: success,
+                error: fail,
+                dataType: "json"
+            });
+
+            function success(obj, stat, xhr) {
+                self.conference(models.Abstract.fromObject(obj));
+            }
+
+            function fail(xhr, stat, msg) {
+                console.log("Error while requesting the abstract: uuid = " + abstrId);
+            }
+
+        };
+
+        self.refresh = function() {
+            var abstr = self.abstract();
+
+            if (self.abstractSaved()) {
+                console.log("save abstract");
+            }
+
+            console.log("refresh abstract");
+            self.abstract(abstr);
         };
     }
 
