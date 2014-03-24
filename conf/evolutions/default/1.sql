4cfe96544583fb7506be71759e84b85ee54cbe62
Christian Kellner <christian@kellner.me>
2014-03-24 16:59:52 +0100

    [sql] Tokens: use TIMESTAMP not DATETIME

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -6,8 +6,8 @@
 CREATE TABLE Tokens (
     id varchar(36) NOT NULL,
     email varchar(255) NOT NULL,
-    creationTime DATETIME NOT NULL,
-    expirationTime DATETIME NOT NULL,
+    creationTime TIMESTAMP NOT NULL,
+    expirationTime TIMESTAMP NOT NULL,
     isSignUp boolean NOT NULL,
     PRIMARY KEY (id)
 );
