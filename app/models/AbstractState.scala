bc70866f2c9b9ec42f309d35129464fdb9e3fcd8
Christian Kellner <christian@kellner.me>
2014-04-01 10:43:41 +0200

    AbstractState: Add state transition maps

--- app/models/AbstractState.scala
+++ app/models/AbstractState.scala
@@ -5,6 +5,24 @@ import javax.persistence.{Converter, AttributeConverter}
 object AbstractState extends Enumeration {
   type State = Value
   val InPreparation, Submitted, InReview, Accepted, Rejected, Published, Withdrawn, InRevision = Value
+
+
+  /* **************************************************************************************************************** *
+   * State transition checking                                                                                        *
+   *                                                                                                                  *
+   *                        ,----- Conference.isOpen                                                                  *
+   *                       \/                                                                                         */
+  val ownerStates = Map("isOpen"  -> Map(InPreparation -> (Submitted :: Nil),
+                                         Submitted     -> (Withdrawn :: Nil),
+                                         Withdrawn     -> (InPreparation :: Nil),
+                                         InRevision    -> (Submitted :: Nil)),
+                       "isClosed" -> Map(InRevision    -> (Submitted :: Nil)))
+
+  val adminStates = Map(Submitted  -> (InReview :: Nil),
+                        InReview   -> (Accepted :: Rejected :: InRevision :: Withdrawn :: Nil),
+                        Accepted   -> (InRevision :: Withdrawn :: Nil),
+                        Rejected   -> (InRevision :: Withdrawn :: Nil),
+                        InRevision -> (InReview :: Nil))
 }
 
 @Converter
