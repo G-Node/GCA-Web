834e89d27d711e87af3aceab2748cb538591fed7
Christian Kellner <christian@kellner.me>
2014-04-01 10:48:35 +0200

    [views+js] adminAbstracts: Add UI for state transitions

--- public/javascripts/adminAbstracts.js
+++ public/javascripts/adminAbstracts.js
@@ -1,4 +1,4 @@
-require(["lib/models", "lib/tools"], function(models, tools) {
+require(["lib/models", "lib/tools", "lib/astate"], function(models, tools, astate) {
     "use strict";
 
 
@@ -17,10 +17,14 @@ require(["lib/models", "lib/tools"], function(models, tools) {
         }
 
         var self = this;
+
         self.message = ko.observable(null);
         self.abstractsData = null;
         self.conference = ko.observable(null);
         self.abstracts = ko.observableArray(null);
+
+        //state related things
+        self.stateHelper = new astate.StateChangeHelper();
 
         self.init = function() {
             self.ensureData();
@@ -47,6 +51,24 @@ require(["lib/models", "lib/tools"], function(models, tools) {
         //helper functions
         self.makeAbstractLink = function(abstract, conference) {
             return "/myabstracts/" + abstract.uuid + "/edit";
+        };
+
+        self.setState = function(abstract, state) {
+            var oldState = abstract.state();
+
+            abstract.state("Saving...");
+            $.ajax("/api/abstracts/" + abstract.uuid + '/state', {
+                data: JSON.stringify({state: state, note: ""}),
+                type: "PUT",
+                contentType: "application/json",
+                success: function(result) {
+                    abstract.state(state);
+                },
+                error: function(jqxhr, textStatus, error) {
+                    abstract.state(oldState);
+                    self.setError("danger", "Error while updating the state", error);
+                }
+            });
         };
 
         self.mkAuthorList = function(abstract) {
@@ -94,6 +116,21 @@ require(["lib/models", "lib/tools"], function(models, tools) {
             //abstract data
             function onAbstractData(absArray) {
                 var absList = models.Abstract.fromArray(absArray);
+
+                absList.forEach(function (abstr) {
+
+                    abstr.makeObservable('state');
+                    abstr.possibleStates = ko.computed(function() {
+                        var fromState = abstr.state();
+
+                        if (fromState === 'Saving...') {
+                            return [];
+                        }
+
+                        return self.stateHelper.getPossibleStatesFor(fromState, true, self.conference().isOpen);
+                    });
+                });
+
                 self.abstractsData = absList;
                 self.abstracts(absList);
 
