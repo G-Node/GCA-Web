ddea125bcdc3d4fce3cbedf878c40b653b3e6730
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2015-01-19 10:49:41 +0100

    BaseCtrlTest: route helper with error handling

--- test/controller/BaseCtrlTest.scala
+++ test/controller/BaseCtrlTest.scala
@@ -1,12 +1,20 @@
 package controller
 
+import conf.Global
 import org.junit.Before
 import org.scalatest.junit.JUnitSuite
-import play.api.test.FakeRequest
+import play.api.Application
+import play.api.http.Writeable
+import play.api.mvc.{Request, SimpleResult}
 import play.api.test.Helpers._
+import play.api.test._
 import securesocial.controllers.ProviderController.authenticateByPost
 import securesocial.core.IdentityId
 import service.Assets
+import scala.concurrent.{ExecutionContext, Future}
+import ExecutionContext.Implicits._
+
+import scala.concurrent.Future
 
 
 trait BaseCtrlTest extends JUnitSuite {
@@ -24,6 +32,17 @@ trait BaseCtrlTest extends JUnitSuite {
     val authResponse = authenticateByPost(id.providerId)(authRequest)
     cookies(authResponse).get("id").getOrElse {
       throw new RuntimeException("Could not authenticate successfully")
+    }
+  }
+
+  /**
+   * Call route and handle errors with Global.onError().
+   */
+  def routeWithErrors[T](app: Application, req: Request[T])(implicit w: Writeable[T]): Option[Future[SimpleResult]] = {
+    route(req).map { result =>
+      result.recoverWith {
+        case t: Throwable => Global.onError(req, t)
+      }
     }
   }
 
