c740a2aa4fb5ab6d8f3d4f5c556e85214738146d
Christian Kellner <christian@kellner.me>
2015-01-22 14:40:27 +0100

    [service] AccountStore: add CredentialsStore

--- app/service/AccountStore.scala
+++ app/service/AccountStore.scala
@@ -3,20 +3,16 @@ package service
 import java.util.{List => JList}
 import javax.persistence.TypedQuery
 
-import anorm.SqlParser._
-import anorm._
+import com.mohiva.play.silhouette.contrib.daos.DelegableAuthInfoDAO
 import com.mohiva.play.silhouette.core.LoginInfo
+import com.mohiva.play.silhouette.core.providers.PasswordInfo
 import com.mohiva.play.silhouette.core.services.IdentityService
-import org.joda.time.DateTime
-import play.api.Play.current
-import play.api.db.DB
-import play.api.{Application, Logger}
-import utils.AnormExtension._
 import plugins.DBUtil._
-import models.{Account, Login}
+import models.{CredentialsLogin, Account, Login}
 
 import scala.collection.JavaConversions._
 import scala.concurrent.Future
+import scala.util.{Success, Failure, Try}
 
 class AccountStore {
   def findByEmail(mail: String): List[Account] = ???
@@ -27,3 +23,28 @@ class LoginStore extends IdentityService[Login]  {
   override def retrieve(loginInfo: LoginInfo): Future[Option[Login]] = ???
 }
 
+
+class CredentialsStore extends DelegableAuthInfoDAO[PasswordInfo] {
+  override def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = ???
+
+  override def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
+
+    val info = query { em =>
+      val queryStr =
+        """SELECT DISTINCT l FROM Login l
+           LEFT JOIN FETCH l.account a
+           WHERE a.mail = :email"""
+
+      val query : TypedQuery[CredentialsLogin] = em.createQuery(queryStr, classOf[CredentialsLogin])
+      query.setParameter("email", loginInfo.providerKey)
+
+      Try(query.getSingleResult)
+    } match {
+      case Success(login) =>
+        Some(PasswordInfo(login.hasher, login.password, None))
+      case Failure(e) => None
+    }
+
+    Future.successful(info)
+  }
+}
