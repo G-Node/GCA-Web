f483bf19296c49cde957fee2277a945d1a385c08
Christian Kellner <christian@kellner.me>
2016-03-01 19:38:55 +0100

    DateTime picker for conference admin dashboard

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,13 +11,15 @@
         paths: {
             requirejs: ["../lib/requirejs/require"],
             jquery: ["../lib/jquery/jquery"],
+            "jquery-ui": ["../lib/jquery-ui/jquery-ui.min"],
             "jquery.ui.sortable": ["../lib/jquery-ui/jquery-ui"],
             knockout: ["//cdnjs.cloudflare.com/ajax/libs/knockout/3.0.0/knockout-debug"],
             "ko.sortable": ["knockout-sortable.min"],
             bootstrap: ["../lib/bootstrap/js/bootstrap"],
             sammy: ["../lib/sammy/sammy"],
             moment: ["../lib/momentjs/moment"],
-            jsroutes: ["/jsroutes"]
+            jsroutes: ["/jsroutes"],
+            datetimepicker: ["//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.1/jquery-ui-timepicker-addon.min"]
         }
     });
 
