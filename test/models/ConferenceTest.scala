694da7a6d0605467e916ee06a6de28fe8663fa7f
Robin Schenk <robin.schenk@campus.lmu.de>
2018-06-12 11:34:02 +0200

    Fixed Sanitising And Added Paragraph Attribute To Markdown Parsing
    Since the Markdown parsing library wraps every string into an HTML paragraph a specific new paragraph class "paragraph-small" had to be introduced to ensure a coherent layout. This attribute is added to all HTML paragraphs in the parsed Markdown data.
    The sanitising had to be adapted slightly to allow classes on HTML paragraph elements, and only there.

--- test/models/ConferenceTest.scala
+++ test/models/ConferenceTest.scala
@@ -24,30 +24,30 @@ class ConferenceTest extends JUnitSuite {
     for (i <- 0 to 10000) {
       testingString = ConferenceTest.generateRandomString(r.nextInt(200)+1)
       // normal text
-      assert(Conference.convertMarkdownToHTML(testingString) == "<p>"+testingString+"</p>\n")
+      assert(Conference.convertMarkdownToHTML(testingString) == "<p class=\"paragraph-small\">"+testingString+"</p>\n")
       // line break
       assert(Conference.convertMarkdownToHTML(testingString+"\\\n"+testingString)
-        == "<p>"+testingString+"<br />\n"+testingString+"</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"<br />\n"+testingString+"</p>\n")
       assert(Conference.convertMarkdownToHTML(testingString+"  \n"+testingString)
-        == "<p>"+testingString+"<br />\n"+testingString+"</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"<br />\n"+testingString+"</p>\n")
       assert(Conference.convertMarkdownToHTML(testingString+"\\"+testingString) // new line is needed for line break
-        == "<p>"+testingString+"\\"+testingString+"</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"\\"+testingString+"</p>\n")
       assert(Conference.convertMarkdownToHTML(testingString+"  "+testingString)
-        == "<p>"+testingString+"  "+testingString+"</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"  "+testingString+"</p>\n")
       assert(Conference.convertMarkdownToHTML(testingString+"\\\n") // followup data is needed for line break
-        == "<p>"+testingString+"\\</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"\\</p>\n")
       // TODO: The parser trims whitespace.
       assert(Conference.convertMarkdownToHTML(testingString+"  \n")
-        == "<p>"+testingString+"</p>\n")
+        == "<p class=\"paragraph-small\">"+testingString+"</p>\n")
       // font
-      assert(Conference.convertMarkdownToHTML("_"+testingString+"_") == "<p><em>"+testingString+"</em></p>\n")
-      assert(Conference.convertMarkdownToHTML("*"+testingString+"*") == "<p><em>"+testingString+"</em></p>\n")
-      assert(Conference.convertMarkdownToHTML("__"+testingString+"__") == "<p><strong>"+testingString+"</strong></p>\n")
-      assert(Conference.convertMarkdownToHTML("**"+testingString+"**") == "<p><strong>"+testingString+"</strong></p>\n")
-      assert(Conference.convertMarkdownToHTML("___"+testingString+"___") == "<p><em><strong>"+testingString+"</strong></em></p>\n"
-      || Conference.convertMarkdownToHTML("___"+testingString+"___") == "<p><strong><em>"+testingString+"</em></strong></p>\n")
-      assert(Conference.convertMarkdownToHTML("***"+testingString+"***") == "<p><em><strong>"+testingString+"</strong></em></p>\n"
-        || Conference.convertMarkdownToHTML("***"+testingString+"***") == "<p><strong><em>"+testingString+"</em></strong></p>\n")
+      assert(Conference.convertMarkdownToHTML("_"+testingString+"_") == "<p class=\"paragraph-small\"><em>"+testingString+"</em></p>\n")
+      assert(Conference.convertMarkdownToHTML("*"+testingString+"*") == "<p class=\"paragraph-small\"><em>"+testingString+"</em></p>\n")
+      assert(Conference.convertMarkdownToHTML("__"+testingString+"__") == "<p class=\"paragraph-small\"><strong>"+testingString+"</strong></p>\n")
+      assert(Conference.convertMarkdownToHTML("**"+testingString+"**") == "<p class=\"paragraph-small\"><strong>"+testingString+"</strong></p>\n")
+      assert(Conference.convertMarkdownToHTML("___"+testingString+"___") == "<p class=\"paragraph-small\"><em><strong>"+testingString+"</strong></em></p>\n"
+      || Conference.convertMarkdownToHTML("___"+testingString+"___") == "<p class=\"paragraph-small\"><strong><em>"+testingString+"</em></strong></p>\n")
+      assert(Conference.convertMarkdownToHTML("***"+testingString+"***") == "<p class=\"paragraph-small\"><em><strong>"+testingString+"</strong></em></p>\n"
+        || Conference.convertMarkdownToHTML("***"+testingString+"***") == "<p class=\"paragraph-small\"><strong><em>"+testingString+"</em></strong></p>\n")
       // heading
       assert(Conference.convertMarkdownToHTML("# "+testingString) == "<h1>"+testingString+"</h1>\n")
       assert(Conference.convertMarkdownToHTML("## "+testingString) == "<h2>"+testingString+"</h2>\n")
@@ -55,35 +55,35 @@ class ConferenceTest extends JUnitSuite {
       assert(Conference.convertMarkdownToHTML("#### "+testingString) == "<h4>"+testingString+"</h4>\n")
       assert(Conference.convertMarkdownToHTML("##### "+testingString) == "<h5>"+testingString+"</h5>\n")
       assert(Conference.convertMarkdownToHTML("###### "+testingString) == "<h6>"+testingString+"</h6>\n")
-      assert(Conference.convertMarkdownToHTML("####### "+testingString) == "<p>####### "+testingString+"</p>\n") // only six headers allowed
-      assert(Conference.convertMarkdownToHTML("#"+testingString) == "<p>#"+testingString+"</p>\n") // whitespace needed
+      assert(Conference.convertMarkdownToHTML("####### "+testingString) == "<p class=\"paragraph-small\">####### "+testingString+"</p>\n") // only six headers allowed
+      assert(Conference.convertMarkdownToHTML("#"+testingString) == "<p class=\"paragraph-small\">#"+testingString+"</p>\n") // whitespace needed
       assert(Conference.convertMarkdownToHTML(testingString + "\n=") == "<h1>"+testingString+"</h1>\n")
       assert(Conference.convertMarkdownToHTML(testingString + "\n-") == "<h2>"+testingString+"</h2>\n")
       // blockquote
-      assert(Conference.convertMarkdownToHTML("> "+testingString) == "<blockquote>\n<p>"+testingString+"</p>\n</blockquote>\n")
-      assert(Conference.convertMarkdownToHTML(">"+testingString) == "<blockquote>\n<p>"+testingString+"</p>\n</blockquote>\n")
+      assert(Conference.convertMarkdownToHTML("> "+testingString) == "<blockquote>\n<p class=\"paragraph-small\">"+testingString+"</p>\n</blockquote>\n")
+      assert(Conference.convertMarkdownToHTML(">"+testingString) == "<blockquote>\n<p class=\"paragraph-small\">"+testingString+"</p>\n</blockquote>\n")
       // lists
       assert(Conference.convertMarkdownToHTML("- "+testingString) == "<ul>\n<li>"+testingString+"</li>\n</ul>\n")
       assert(Conference.convertMarkdownToHTML("+ "+testingString) == "<ul>\n<li>"+testingString+"</li>\n</ul>\n")
       assert(Conference.convertMarkdownToHTML("* "+testingString) == "<ul>\n<li>"+testingString+"</li>\n</ul>\n")
-      assert(Conference.convertMarkdownToHTML("-"+testingString) == "<p>-"+testingString+"</p>\n") // space needed
-      assert(Conference.convertMarkdownToHTML("+"+testingString) == "<p>+"+testingString+"</p>\n") // space needed
-      assert(Conference.convertMarkdownToHTML("*"+testingString) == "<p>*"+testingString+"</p>\n") // space needed
+      assert(Conference.convertMarkdownToHTML("-"+testingString) == "<p class=\"paragraph-small\">-"+testingString+"</p>\n") // space needed
+      assert(Conference.convertMarkdownToHTML("+"+testingString) == "<p class=\"paragraph-small\">+"+testingString+"</p>\n") // space needed
+      assert(Conference.convertMarkdownToHTML("*"+testingString) == "<p class=\"paragraph-small\">*"+testingString+"</p>\n") // space needed
       assert(Conference.convertMarkdownToHTML("- "+testingString+"\n- "+testingString)
         == "<ul>\n<li>"+testingString+"</li>\n<li>"+testingString+"</li>\n</ul>\n")
       assert(Conference.convertMarkdownToHTML("1. "+testingString) == "<ol>\n<li>"+testingString+"</li>\n</ol>\n")
-      assert(Conference.convertMarkdownToHTML("1."+testingString) == "<p>1."+testingString+"</p>\n") // space needed
-      assert(Conference.convertMarkdownToHTML("2)"+testingString) == "<p>2)"+testingString+"</p>\n") // space needed
+      assert(Conference.convertMarkdownToHTML("1."+testingString) == "<p class=\"paragraph-small\">1."+testingString+"</p>\n") // space needed
+      assert(Conference.convertMarkdownToHTML("2)"+testingString) == "<p class=\"paragraph-small\">2)"+testingString+"</p>\n") // space needed
       assert(Conference.convertMarkdownToHTML("1. "+testingString+"\n2. "+testingString)
         == "<ol>\n<li>"+testingString+"</li>\n<li>"+testingString+"</li>\n</ol>\n")
       // links
       val urlTestingString : String = "http:"+testingString
       assert(Conference.convertMarkdownToHTML("<"+urlTestingString+">")
-        == "<p><a href=\""+urlTestingString+"\">"+urlTestingString+"</a></p>\n")
+        == "<p class=\"paragraph-small\"><a href=\""+urlTestingString+"\">"+urlTestingString+"</a></p>\n")
       assert(Conference.convertMarkdownToHTML("["+testingString+"]"+"("+urlTestingString+")")
-        == "<p><a href=\""+urlTestingString+"\">"+testingString+"</a></p>\n")
+        == "<p class=\"paragraph-small\"><a href=\""+urlTestingString+"\">"+testingString+"</a></p>\n")
       // code
-      assert(Conference.convertMarkdownToHTML("`"+testingString+"`") == "<p><code>"+testingString+"</code></p>\n")
+      assert(Conference.convertMarkdownToHTML("`"+testingString+"`") == "<p class=\"paragraph-small\"><code>"+testingString+"</code></p>\n")
       assert(Conference.convertMarkdownToHTML("    "+testingString) == "<pre><code>"+testingString+"\n</code></pre>\n")
       assert(Conference.convertMarkdownToHTML("```\n"+testingString+"\n```") == "<pre><code>"+testingString+"\n</code></pre>\n")
     }
@@ -113,6 +113,19 @@ class ConferenceTest extends JUnitSuite {
     testingConf = Conference(Some("uuid"), Some("wrongconf"), Some("XX"), Some("G"), Some("X"),
       None, None, Some(false), Some(true), Some(false), Some(true), None, None, None, info = null)
     assert(testingConf.getInfoAsHTML() == "")
+    // test sanitising
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, None, Some(false), Some(true), Some(false), Some(true), None, None, None,
+      info = Some("<h1 class=\"paragraph-small\">Some text</h1>"))
+    assert(testingConf.getInfoAsHTML() == "<h1>Some text</h1>\n") // attributes are not allowed
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, None, Some(false), Some(true), Some(false), Some(true), None, None, None,
+      info = Some("Some text"))
+    assert(testingConf.getInfoAsHTML() == "<p class=\"paragraph-small\">Some text</p>\n") // except classes on paragraphs
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, None, Some(false), Some(true), Some(false), Some(true), None, None, None,
+      info = Some("<script type=\"text/javascript\">alert(\"Cross-Site-Scripting\");</script>"))
+    assert(testingConf.getInfoAsHTML() == "\n") // everything else should also be disallowed
   }
 
   @Test
@@ -139,6 +152,16 @@ class ConferenceTest extends JUnitSuite {
     testingConf = Conference(Some("uuid"), Some("wrongconf"), Some("XX"), Some("G"), Some("X"),
       None, null, Some(false), Some(true), Some(false), Some(true), None, None, None)
     assert(testingConf.getDescriptionAsHTML() == "")
+    // test sanitising
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, Some("<h1 class=\"paragraph-small\">Some text</h1>"), Some(false), Some(true), Some(false), Some(true), None, None, None)
+    assert(testingConf.getDescriptionAsHTML() == "<h1>Some text</h1>\n") // attributes are not allowed
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, Some("Some text"), Some(false), Some(true), Some(false), Some(true), None, None, None)
+    assert(testingConf.getDescriptionAsHTML() == "<p class=\"paragraph-small\">Some text</p>\n") // except classes on paragraphs
+    testingConf = Conference(Some("uuid"), Some("someconf"), Some("XX"), Some("G"), Some("X"),
+      None, Some("<script type=\"text/javascript\">alert(\"Cross-Site-Scripting\");</script>"), Some(false), Some(true), Some(false), Some(true), None, None, None)
+    assert(testingConf.getDescriptionAsHTML() == "\n") // everything else should also be disallowed
   }
 
 }
