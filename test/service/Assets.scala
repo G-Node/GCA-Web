8bb97c865f120e6160b311049eb41b39f381209f
fschrader1992 <fschrader1992@gmail.com>
2018-09-11 16:00:18 +0200

    First version that has no building problems.

--- test/service/Assets.scala
+++ test/service/Assets.scala
@@ -279,6 +279,8 @@ class Assets() {
         abstr.owners.add(alice)
         abstr.owners.add(bob)
 
+        abstr.favUsers.add(alice)
+
         abstr.authors.foreach { author =>
           author.abstr = abstr
         }
