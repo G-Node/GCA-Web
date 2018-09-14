d3588ec06e2d001a463e52d74e48078170498cf2
Robin Schenk <robin.schenk@campus.lmu.de>
2018-09-14 20:01:47 +0200

    Updated AppCache Manifest
    The AppCache manifest for compatibility with older browsers is now generated dynamically by the backend.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,7 @@ import models._
 import service.{AbstractService, ConferenceService}
 import java.net._
 import org.joda.time.DateTime
+import scala.collection.JavaConverters._
 
 class Application(implicit val env: Environment[Login, CachedCookieAuthenticator])
   extends Silhouette[Login, CachedCookieAuthenticator] {
@@ -156,6 +157,122 @@ class Application(implicit val env: Environment[Login, CachedCookieAuthenticator
     }
 
     Ok(views.html.abstractviewer(request.identity.map{ _.account }, abstr.conference, abstr))
+  }
+
+  def createAppCacheManifest() = UserAwareAction { implicit request =>
+    var dynamicViews: String = "";
+    conferenceService.list().foreach(conf => {
+      dynamicViews += s"""
+           |/conference/${conf.short}
+           |/conference/${conf.short}/schedule
+           |/conference/${conf.short}/submission
+           |/conference/${conf.short}/floorplans
+           |/conference/${conf.short}/locations
+           |/conference/${conf.short}/abstracts
+           |${conf.logo}
+           |${conf.thumbnail}""".stripMargin
+      for (abs: Abstract <- conf.abstracts.asScala) {
+        dynamicViews +=
+          s"""
+             |/abstracts/${abs.uuid}""".stripMargin
+      }
+    })
+
+    Ok(
+      s"""CACHE MANIFEST
+         |# v1.0.1
+         |# Views
+         |/
+         |/conferences
+         |/contact
+         |/about
+         |/impressum
+         |/login
+         |# Assets
+         |/assets/lib/momentjs/moment.js
+         |/assets/lib/bootstrap/js/bootstrap.min.js
+         |/assets/lib/bootstrap/js/bootstrap.js
+         |/assets/stylesheets/g-node-bootstrap.play.css
+         |/assets/lib/jquery/jquery.js
+         |/assets/lib/jquery/jquery.min.js
+         |/assets/lib/jquery-ui/jquery-ui.min.css
+         |/assets/lib/jquery-ui/jquery-ui.js
+         |/assets/stylesheets/layout.css
+         |/assets/javascripts/require.js
+         |/assets/lib/requirejs/require.js
+         |/assets/lib/sammy/sammy.js
+         |/assets/images/favicon.png
+         |/assets/images/bccn.png
+         |/assets/images/gnode_logo.png
+         |/assets/fonts/glyphicons-halflings-regular.eot
+         |/assets/fonts/glyphicons-halflings-regular.svg
+         |/assets/fonts/glyphicons-halflings-regular.ttf
+         |/assets/fonts/glyphicons-halflings-regular.woff
+         |/assets/javascripts/knockout-sortable.min.js
+         |# leaflet
+         |/assets/javascripts/lib/leaflet/leaflet.css
+         |/assets/javascripts/lib/leaflet/leaflet.js
+         |/assets/javascripts/lib/leaflet/leaflet-src.js
+         |/assets/javascripts/lib/leaflet/images/layers.png
+         |/assets/javascripts/lib/leaflet/images/layers-2x.png
+         |/assets/javascripts/lib/leaflet/images/marker-icon.png
+         |/assets/javascripts/lib/leaflet/images/marker-icon-2x.png
+         |/assets/javascripts/lib/leaflet/images/marker-shadow.png
+         |# scheduler
+         |/assets/javascripts/lib/scheduler/dhtmlxscheduler.css
+         |/assets/javascripts/lib/scheduler/dhtmlxscheduler.js
+         |/assets/javascripts/lib/scheduler/ext/dhtmlxscheduler_readonly.js
+         |# libs
+         |/assets/javascripts/lib/accessors.js
+         |/assets/javascripts/lib/astate.js
+         |/assets/javascripts/lib/models.js
+         |/assets/javascripts/lib/msg.js
+         |/assets/javascripts/lib/multi.js
+         |/assets/javascripts/lib/offline.js
+         |/assets/javascripts/lib/owned.js
+         |/assets/javascripts/lib/tools.js
+         |/assets/javascripts/lib/update-storage.js
+         |/assets/javascripts/lib/validate.js
+         |# View Models
+         |/assets/javascripts/abstract-list.js
+         |/assets/javascripts/abstract-viewer.js
+         |/assets/javascripts/browser.js
+         |/assets/javascripts/conference-schedule.js
+         |/assets/javascripts/config.js
+         |/assets/javascripts/editor.js
+         |/assets/javascripts/locations.js
+         |/assets/javascripts/main.js
+         |/assets/javascripts/userdash.js
+         |
+         |https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.1/jquery-ui-timepicker-addon.min.js
+         |https://cdnjs.cloudflare.com/ajax/libs/knockout/3.0.0/knockout-debug.js
+         |https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.1/jquery-ui-timepicker-addon.min.css
+         |https://fonts.googleapis.com/css?family=EB+Garamond|Open+Sans
+         |https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.3/MathJax.js?delayStartupUntil=configured
+         |https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.3/extensions/MathMenu.js
+         |
+         |# Styles
+         |/assets/stylesheets/_g-node-bootstrap.less
+         |/assets/stylesheets/g-node-bootstrap.play.less
+         |/assets/stylesheets/layout.less
+         |/assets/stylesheets/Readme.md
+         |/assets/stylesheets/custom/_classes.less
+         |/assets/stylesheets/custom/_classes_conference_scheduler.less
+         |/assets/stylesheets/custom/_colors.less
+         |/assets/stylesheets/custom/_font.less
+         |/assets/stylesheets/custom/bootstrap/_custom-colors.less
+         |/assets/stylesheets/custom/bootstrap/_custom-elements.less
+         |/assets/stylesheets/custom/bootstrap/_custom-fonts.less
+         |/assets/stylesheets/custom/bootstrap/_custom-vars.less
+         |
+         |# Dynamic Views
+         |${dynamicViews}
+         |
+         |NETWORK:
+         |*
+         |http:/*
+         |https:/*
+        """.stripMargin).as("text/cache-manifest")
   }
 
 }
