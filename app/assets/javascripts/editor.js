fe0dccdae7cc9f64efe8cb03131ad3d8a2a86e1b
fschrader1992 <fschrader1992@gmail.com>
2019-01-23 10:53:13 +0100

    Revert "[js/editor] Abstract Submission Without Topics Defined"
    This reverts commit d20ecf9

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -146,16 +146,6 @@ function (ko, models, tools, msg, validate, owned, astate) {
             }
         };
 
-        self.checkRemoveTopics = function (warnings) {
-            if (self.conference().topics === null || self.conference().topics.length === 0) {
-                warnings.forEach(function (currWarning) {
-                    if (currWarning.match("topic")) {
-                        warnings.splice(warnings.indexOf(currWarning), 1);
-                    }
-                });
-            }
-        };
-
         self.validity = ko.computed(
             function() {
                 var abstract = self.abstract();
@@ -191,7 +181,6 @@ function (ko, models, tools, msg, validate, owned, astate) {
                     };
                 } else {
                     self.checkRemovePresPref(res.warnings);
-                    self.checkRemoveTopics(res.warnings);
                     var nwarn = res.warnings.length;
                     return {
                         ok: false,
@@ -663,7 +652,6 @@ function (ko, models, tools, msg, validate, owned, astate) {
                 var result = validate.abstract(self.abstract());
 
                 self.checkRemovePresPref(result.warnings);
-                self.checkRemoveTopics(res.warnings);
                 if (!result.ok()) {
                     self.setError("Error", "Unable to submit: " +
                         (result.hasErrors() ? result.errors[0] : result.warnings[0]));
