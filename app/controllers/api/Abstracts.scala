0d866b3ffca360a2112e7a3c85c7ef1297db98d9
fschrader1992 <fschrader1992@gmail.com>
2019-03-04 19:38:00 +0100

    [Abstracts.scala] Remove (parse.json) from AddFavUser
    Not necessary for function to work properly.

--- app/controllers/api/Abstracts.scala
+++ app/controllers/api/Abstracts.scala
@@ -272,7 +272,7 @@ extends Silhouette[Login, CachedCookieAuthenticator] {
     *
     * @return The id of the updated Abstract as JSON
     */
-  def addFavUser(id: String) = SecuredAction(parse.json) { implicit request =>
+  def addFavUser(id: String) = SecuredAction { implicit request =>
     val abstr = abstractService.get(id)
     abstractService.addFavUser(abstr, request.identity.account)
     Ok(Json.toJson(id))
