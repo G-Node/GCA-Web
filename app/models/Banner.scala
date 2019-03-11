9eb8bb486719d5695e8f36299ea26471ac4b76b0
fschrader1992 <fschrader1992@gmail.com>
2019-03-11 11:25:11 +0100

    [Banners] Add Banner Model
    Add Banner model for upload of conference graphics.
    Related to issue #385.

---
+++ app/models/Banner.scala
@@ -0,0 +1,39 @@
+// Copyright © 2014, German Neuroinformatics Node (G-Node)
+//                   A. Stoewer (adrian.stoewer@rz.ifi.lmu.de)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package models
+
+import models.Model._
+import javax.persistence.{ManyToOne, Entity}
+
+/**
+  * A model for banners.
+  */
+@Entity
+class Banner extends Model {
+
+  @ManyToOne
+  var conference: Conference = _
+
+}
+
+object Banner {
+
+  def apply(uuid: Option[String],
+            conference: Option[Conference] = None) : Banner = {
+
+    val banner = new Banner()
+
+    banner.uuid     = unwrapRef(uuid)
+    banner.conference    = unwrapRef(conference)
+
+    banner
+  }
+
+}
