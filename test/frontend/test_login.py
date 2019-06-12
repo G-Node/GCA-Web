e287017c7c640a70223eef6d065ed76ce961d18a
fschrader1992 <fschrader1992@gmail.com>
2019-06-12 11:44:27 +0200

    [selenium/test_login.py] Adopt Fixture/Cross Browser Support
    Adding the fixture mechanism to the test class in order to enable
    parallel cross browser testing.

--- test/frontend/test_login.py
+++ test/frontend/test_login.py
@@ -1,16 +1,12 @@
-import unittest
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import Cookies
 
 
-class LoginTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(inst):
-        inst.driver = webdriver.Chrome()
-        inst.driver.get("http://localhost:9000/login")
-        inst.driver.maximize_window()
+@pytest.mark.usefixtures("setup_login")
+class TestLogin:
 
     def test_login(self, user="alice@foo.com", password="testtest"):
         driver = self.driver
@@ -37,14 +33,11 @@ class LoginTest(unittest.TestCase):
 
     def test_logout(self):
         driver = self.driver
+        Cookies.load_cookies(driver)
+        driver.get("http://" + Cookies.get_host_ip() + ":9000/conferences")
+        WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="usermenu"]'))
+        )
         driver.find_element_by_id("usermenu").click()
         driver.find_element_by_partial_link_text("Logout").click()
-        self.assertTrue(driver.find_element_by_partial_link_text("Login"))
-
-    @classmethod
-    def tearDownClass(inst):
-        inst.driver.close()
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert driver.find_element_by_partial_link_text("Login")
