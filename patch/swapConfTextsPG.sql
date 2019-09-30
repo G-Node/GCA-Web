a8a7c1973a7218a02c5ba36fc87c6950c0ae2068
fschrader1992 <fschrader1992@gmail.com>
2019-09-30 13:52:15 +0200

    [patch/swapConfTextsPG.sql] Remove CREATE TABLE
    As OMR creates table. UUIDs allocated in INSERT statements.

--- patch/swapConfTextsPG.sql
+++ patch/swapConfTextsPG.sql
@@ -12,24 +12,24 @@ CREATE TABLE IF NOT EXISTS conftext(
     conference_uuid varchar(255) NOT NULL
 );
 
-INSERT INTO conftext (cttype, "text", conference_uuid)
-SELECT 'description', description, uuid FROM conference
+INSERT INTO conftext (uuid, cttype, "text", conference_uuid)
+SELECT uuid_generate_v1(), 'description', description, uuid FROM conference
 WHERE description IS NOT NULL AND NOT EXISTS (
     SELECT cttype, conference_uuid FROM conftext
     WHERE conftext.conference_uuid = conference.uuid
     AND conftext.cttype = 'description'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO conftext (cttype, text, conference_uuid)
-SELECT 'logo', logo, uuid FROM conference
+INSERT INTO conftext (uuid, cttype, text, conference_uuid)
+SELECT uuid_generate_v1(), 'logo', logo, uuid FROM conference
 WHERE logo IS NOT NULL AND NOT EXISTS (
     SELECT cttype, conference_uuid FROM conftext
     WHERE conftext.conference_uuid = conference.uuid
     AND conftext.cttype = 'logo'
 ) ON CONFLICT DO NOTHING;
 
-INSERT INTO conftext (cttype, text, conference_uuid)
-SELECT 'thumbnail', thumbnail, uuid FROM conference
+INSERT INTO conftext (uuid, cttype, text, conference_uuid)
+SELECT uuid_generate_v1(), 'thumbnail', thumbnail, uuid FROM conference
 WHERE thumbnail IS NOT NULL AND NOT EXISTS (
     SELECT cttype, conference_uuid FROM conftext
     WHERE conftext.conference_uuid = conference.uuid
