898502a22650d3947af1b4c9d3bb679b00fd6f35
M. Sonntag <michael.p.sonntag@gmail.com>
2020-01-08 18:15:31 +0100

    [FigureService] Add openMobileFile method
    The FigureMobileService has only one method ('open').
    Since FigureService also already contains the mobile path,
    this method is migrated to FigureMobile.

--- app/service/FigureService.scala
+++ app/service/FigureService.scala
@@ -259,6 +259,31 @@ class FigureService(figPath: String, figMobilePath: String) {
     file
   }
 
+  /**
+   * Open the mobile image file that belongs to the figure;
+   * if the mobile figure cannot be found, fallback to the
+   * original figure.
+   *
+   * @param fig The figure to open.
+   *
+   * @return A file handler to the respective image file.
+   */
+  def openMobileFile(fig: Figure) : File = {
+    if (fig.uuid == null)
+      throw new IllegalArgumentException("Unable to open file for figure without uuid")
+
+    var file = new File(figMobilePath, fig.uuid)
+
+    // If a lowres file cannot be found, get back to the original file
+    if (!file.exists || !file.canRead)
+      file = new File(figPath, fig.uuid)
+
+    if (!file.exists || !file.canRead)
+      throw new FileNotFoundException("Unable to open the file for reading: " + file.toString)
+
+    file
+  }
+
 }
 
 /**
