7df641cebccdc5de08c87eb7d98dbd5a289c2c54
fschrader1992 <fschrader1992@gmail.com>
2020-02-07 14:59:45 +0100

    [test_conference_creation] Add test_short

--- test/frontend/test_conference_creation.py
+++ test/frontend/test_conference_creation.py
@@ -19,3 +19,25 @@ class TestConferenceCreation:
         assert "TC" in element_get_attribute_by_id(driver, 'short', 'value')
         assert "500" == element_get_attribute_by_id(driver, 'mAbsLen', 'value')
         assert "0" == element_get_attribute_by_id(driver, 'mFigs', 'value')
+
+    def test_short(self):
+        driver = self.driver
+        tc_num = "TC" + str(uuid1())[0:8]
+        element_send_keys_by_id(driver, 'short', 'BC14')
+
+        element_click_by_class_name(driver, 'btn-success')
+
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//div[contains(@class,"alert-danger")]/strong'))
+        )
+        assert "Conference short is already in use." in \
+               driver.find_element_by_xpath('//div[contains(@class,"alert-danger")]/strong').text
+
+        element_send_keys_by_id(driver, 'short', tc_num)
+
+        element_click_by_class_name(driver, 'btn-success')
+
+        WebDriverWait(driver, 30).until(
+            EC.invisibility_of_element_located((By.XPATH, '//div[contains(@class,"alert-danger")]/strong'))
+        )
+        assert len(driver.find_elements_by_xpath('//div[contains(@class,"alert-danger")]/strong')) == 0
